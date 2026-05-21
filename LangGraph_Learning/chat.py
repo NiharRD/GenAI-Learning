2ee608f7bf915d686bca79979fe97a6a7d599e5e
NihaR_RD <niharranjandas008@gmail.com>
2026-05-21 21:15:43 +0530

    hehe

---
+++ LangGraph_Learning/chat.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from typing_extensions import TypedDict
+from typing import Annotated
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+from langchain_openai import ChatOpenAI
+load_dotenv()
+
+llm=ChatOpenAI(model="gpt-4o")
+
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+## State is nothing but a list of annoated messaged
+
+def chatbot(state: State):
+    response = llm.invoke(state.get("messages"))
+    return { "messages": [response] }
+def samplenode(state: State):
+    print("\n\nInside samplenode node", state)
+    return { "messages": ["Sample Message Appended"] }
+
+graph_builder = StateGraph(State) ## Providing initial state to the graph builder, which will be used to validate the state at each node and edge.
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("samplenode", samplenode)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", "samplenode")
+graph_builder.add_edge("samplenode", END)
+
+graph = graph_builder.compile()
+
+updated_state = graph.invoke(State({"messages": ["What is my name?"]}))
+print("\n\nupdated_state", updated_state)
+
+# (START) -> chatbot -> samplenode -> (END)
+
+# state = { messages: ["Hey there"] }
+# node runs: chatbot(state: ["Hey There"]) -> ["Hi, This is a message from ChatBot Node"]
+# state = { "messages": ["Hey there", "Hi, This is a message from ChatBot Node"]  }
---
+++ LangGraph_Learning/imp.md
@@ -0,0 +1,3 @@
+Regular Nodes (Functions): Return a dictionary of {"state_key": "new_value"}.
+
+Conditional Edges (Routing): Return a string of "next_node_name".
